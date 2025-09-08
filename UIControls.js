8563eac4c292e60d23d59385712dd23c8cb5d7bb
Ameliasks <s4102385@student.rmit.edu.au>
2025-09-08 19:01:30 +1000

    added button and slider

--- UIControls.js
+++ UIControls.js
@@ -10,3 +10,14 @@ document.getElementById("dialogCloseButton").addEventListener("click", () => {
 });
 
 introModal.addEventListener("close", toneInit);
+
+// volume slider
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
